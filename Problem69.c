e55db4b7dbddddd063e00ae34e3ed4d338f08704
Aditya <55033839+AGVGA@users.noreply.github.com>
2020-08-06 19:36:22 +0530

    Update Problem69.c

--- Problem69.c
+++ Problem69.c
@@ -1,10 +1,12 @@
-/*Problem 69
+/* Problem 69
 Function written in C language
-All testcases passed*/
+All testcases passed */
 
 int mySqrt(int x)
 {
     int i;
+    /* Use the sqrt() function in C language and assign it to
+       an integer variable so that it's value gets truncated */
     i = sqrt(x);
     return i;
 }
