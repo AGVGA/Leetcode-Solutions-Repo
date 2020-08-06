c68f3598e43572fbcaa52866b7e3e51621fc0def
Aditya <55033839+AGVGA@users.noreply.github.com>
2020-08-06 19:18:30 +0530

    Update Problem09.c

--- Problem09.c
+++ Problem09.c
@@ -7,12 +7,15 @@ bool isPalindrome(int x)
     int x1, rem, result;
     long int y = 0;
     
+    /* Converting given number into it's reverse number */
     for(x1 = x; x1 > 0;  x1 /= 10)
     {
         rem = x1 % 10;
         y = y * 10 + rem;
     }
     
+    /* Checking if the given number and it's reverse number
+       are the same or not */
     if(x == y)
     {
         return true;
