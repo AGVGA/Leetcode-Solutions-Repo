f71655f492e5e71255e285ae32446edad6f61f86
Aditya <55033839+AGVGA@users.noreply.github.com>
2020-08-06 19:41:20 +0530

    Update Problem09.c

--- Problem09.c
+++ Problem09.c
@@ -1,20 +1,20 @@
-/*Problem 9
+/* Problem 9
 Function written in C language
-All testcases passed*/
+All testcases passed */
 
 bool isPalindrome(int x)
 {
     int x1, rem, result;
     long int y = 0;
     
-    /* Converting given number into it's reverse number */
+    /* Convert given number into it's reverse number */
     for(x1 = x; x1 > 0;  x1 /= 10)
     {
         rem = x1 % 10;
         y = y * 10 + rem;
     }
     
-    /* Checking if the given number and it's reverse number
+    /* Check if the given number and it's reverse number
        are the same or not */
     if(x == y)
     {
