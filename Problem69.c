3a81b3fd8b6ebb12fd4e5f4b9d9f2261b94cc192
Aditya <55033839+AGVGA@users.noreply.github.com>
2020-08-07 14:09:36 +0530

    Update Problem69.c

--- Problem69.c
+++ Problem69.c
@@ -5,8 +5,8 @@ All testcases passed */
 int mySqrt(int x)
 {
     int i;
-    /* Use the sqrt() function in C language and assign it to
-       an integer variable so that it's value gets truncated */
+    /** Use the sqrt() function in C language and assign it to
+        an integer variable so that it's value gets truncated **/
     i = sqrt(x);
     return i;
 }
