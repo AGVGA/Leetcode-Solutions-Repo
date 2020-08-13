2200bcc4ffd8ca1e767c884db26b03f652c63ab5
Aditya <55033839+AGVGA@users.noreply.github.com>
2020-08-13 23:07:35 +0530

    Create Problem342.c

---
+++ Problem342.c
@@ -0,0 +1,29 @@
+/*! Problem 342
+Function written in C language
+All testcases passed */
+
+bool isPowerOfFour(int num)
+{
+    /** Check if input is negative **/
+    if(num <= 0)
+    {
+        return false;
+    }
+    
+    /** Divide input with four until remainder is zero **/
+    for( ; num % 4 == 0;  )
+    {
+        /** Update input with it's quotient **/
+        num = num / 4;
+    }
+    
+    /** Check for quotient **/
+    if(num == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
